2c4644262a60e562f462e8a49c2774860a8f79eb
mt5225 <mt5225@gmail.com>
2016-01-12 15:16:36 -0800

    add hpsa adapter

---
+++ HPSAAdapter.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+from arc_te.celery import app
+import spur
+import spur.ssh
+
+
+@app.task
+def run(**kwargs):
+    shell = spur.SshShell(
+        hostname=kwargs['hpsa_hostname'],
+        port=int(kwargs['hpsa_port']),
+        username=kwargs['hpsa_username'],
+        password=kwargs['hpsa_password'],
+        connect_timeout=kwargs['timeout'],
+        missing_host_key=spur.ssh.MissingHostKey.accept
+    )
+    with shell:
+        commamd = "/opsw/bin/rosh -l %s -n %s %s" % (kwargs['target_username'], kwargs['target'], kwargs['command'])
+        result = shell.run(commamd)
+        return result.output
